24ea36244890ca47b418eaaf5ddaca9fc0f44c4d
Berkant Arslan Arduç <berkantarduc@hacettepe.edu.tr>
2022-12-27 21:31:53 +0300

    Projenin %30 Luk kısmı tamamlandı

--- Kullanici/KullaniciEkleForm.Designer.cs
+++ Kullanici/KullaniciEkleForm.Designer.cs
@@ -45,7 +45,6 @@
             this.radioK = new System.Windows.Forms.RadioButton();
             this.kaydetbtn = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -191,17 +190,8 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(282, 27);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(707, 498);
+            this.dataGridView1.Size = new System.Drawing.Size(707, 513);
             this.dataGridView1.TabIndex = 16;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(0, 0);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(35, 13);
-            this.label8.TabIndex = 17;
-            this.label8.Text = "label8";
             // 
             // KullaniciEkleForm
             // 
@@ -209,7 +199,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(1001, 566);
-            this.Controls.Add(this.label8);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.kaydetbtn);
             this.Controls.Add(this.radioK);
@@ -255,6 +244,5 @@
         private System.Windows.Forms.RadioButton radioK;
         private System.Windows.Forms.Button kaydetbtn;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Label label8;
     }
 }
